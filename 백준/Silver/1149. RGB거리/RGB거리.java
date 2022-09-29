e3c7f82cf8a1d70ed5ba2cade913bda000859ab0
ohuiseok <109773298+ohuiseok@users.noreply.github.com>
2022-09-29 11:12:05 +0900

    [Silver I] Title: RGB거리, Time: 136 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,31 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	static int N;
+	static int[][] color;
+	static int[][] memo;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-		int[][] arr = new int[T + 1][3];
-
-		for (int t = 1; t <= T; t++) {
-			st = new StringTokenizer(br.readLine());
-			int r = Integer.parseInt(st.nextToken());
-			int g = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			arr[t][0] = Integer.min(arr[t - 1][1], arr[t - 1][2]) + r;
-			arr[t][1] = Integer.min(arr[t - 1][0], arr[t - 1][2]) + g;
-			arr[t][2] = Integer.min(arr[t - 1][0], arr[t - 1][1]) + b;
+		N = Integer.parseInt(br.readLine());
+		color = new int[N+1][3];
+		memo = new int[N+1][3];
+		int answer;
+		
+		for(int t=1;t<=N;t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i=0;i<3;i++)
+				color[t][i] = Integer.parseInt(st.nextToken());
+			memo[t][0] = color[t][0] + Integer.min(memo[t-1][1],memo[t-1][2]);
+			memo[t][1] = color[t][1] +Integer.min(memo[t-1][0],memo[t-1][2]);
+			memo[t][2] = color[t][2] +Integer.min(memo[t-1][0],memo[t-1][1]);
 		}
-		int tmp = Integer.min(arr[T][0], arr[T][1]);
-		System.out.println(Integer.min(tmp, arr[T][2]));
+	
+		answer = Integer.min(memo[N][0], memo[N][1]);
+		answer = Integer.min(answer, memo[N][2]);
+		System.out.println(answer);
 	}
 
+	/*
+	 * 하향식 방식
+	 * r 선택 + 이전에서 g선택시최소값, r 선택 + 이전에서b선택최소값
+	 * g 선택 + 이전에서 r선택시최소값, g 선택 + 이전에서b선택최소값
+	 * b 선택 + 이전에서 r선택시최소값, b 선택 + 이전에서g선택최소값
+	 * 이 세가지 중 가장 최소값 호출.
+	 * 
+	 * 코드로 작성하려니 어렵다. 
+	 * 
+	 * 상향식으로 변환해보자
+	 * 첫번째 DP배열에는 R,G,B초기값을 넣는다.
+	 * 두번째 DP배열에는 "현재 R을 골랐을 때 + (이전에 G,B값 중 최소값)"
+	 * 				"현재 G을 골랐을 때 + (이전에 R,B값 중 최소값)"
+	 * 				"현재 B을 골랐을 때 + (이전에 R,G값 중 최소값)" 
+	 * 이렇게 나아간다.
+	 * 
+	 * 
+	 * 
+	 * */
 }
