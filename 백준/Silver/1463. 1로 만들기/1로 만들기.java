27e7eff92e34e6bf352f125d810f08b74982b328
ohuiseok <109773298+ohuiseok@users.noreply.github.com>
2022-09-29 14:27:18 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18172 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,55 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
-	/*
-	 * 다이나믹 재귀 배열을 만들어 놓고, 재귀함수 내에서 배열이 null 일때만 내부 재귀를 타고 들어가게 한다. 특정 조건에 값넣기
-	 */
-
-	static Integer[] dp;
-
-	public static int makeOne(int value) {
-		if (dp[value] != null)
-			return dp[value]; 
-
-		int a = Integer.MAX_VALUE;
-		int b = Integer.MAX_VALUE;
-		int c = Integer.MAX_VALUE;
-		
-		if (value % 3 == 0)
-			a = makeOne(value / 3) + 1;
-
-		if (value % 2 == 0)
-			b = makeOne(value / 2) + 1;
-
-		if (value - 1 >= 0)
-			c = makeOne(value - 1) + 1;
-
-		a = Integer.min(a, b);
-		
-		dp[value] = Integer.min(a, c);
-		return dp[value];
-		
-	}
-
-	public static void main(String[] args) throws IOException {
+	static int memo[] = new int[1000000];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-		dp = new Integer[T + 1];
-		dp[1] = 0;
+		int first = Integer.parseInt(br.readLine());
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(first);
+		int cnt = 0;
 		
-		makeOne(T);
-
-		System.out.println(dp[T]);
-
+		
+		while(!queue.isEmpty()) {
+			cnt ++;
+			Queue<Integer> newQueue = new LinkedList<Integer>();
+			
+			while(!queue.isEmpty()) {
+				int value = queue.poll();
+//				System.out.print(value+" ");
+				if( value==1)
+				{
+					System.out.println(cnt-1);
+					return;
+				}
+				
+				if(value%3==0 && memo[value/3] == 0) {
+					memo[value/3] = cnt;
+					newQueue.add(value/3);
+				}
+				if(value%2==0 && memo[value/2] == 0) {
+					memo[value/2] = cnt;
+					newQueue.add(value/2);
+				}
+				if(value-1>0 && memo[value-1]==0) {
+					memo[value-1] = cnt;
+					newQueue.add(value-1);
+					
+				}
+					
+			}
+//			System.out.println();
+			queue = newQueue;
+		}
+		
 	}
 
+	
 }
