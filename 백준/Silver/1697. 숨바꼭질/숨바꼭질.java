72401ada23b42c2c5b33d39bf80a7568f3e7e64f
uiseok <109773298+ohuiseok@users.noreply.github.com>
2023-11-21 18:00:00 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 18512 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,72 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
-import java.util.StringTokenizer;
-
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
-		int answer = 0;
-		boolean success = false;
-		Queue<Integer> list = new LinkedList<Integer>();
-		Set<Integer> store = new HashSet<Integer>();
-		
-		if(start == end) {
-			System.out.println(0);
-			return;
-		}
-		
-		if(start>0) {
-			list.add(start-1);
-			store.add(start-1);
-		}
-		list.add(start+1);
-		list.add(start*2);
-		store.add(start+1);
-		store.add(start*2);
-		
-		while(true) {
-			answer++;
-			int size = list.size();
-			for(int i=0;i<size;i++) {
-				int a = list.poll();
-				if(a==end) {
-					success= true;
-					break;
-				}
-				if(a-1 >=0 &&  store.add(a-1)) {
-					list.add(a-1);
-				}
-				if(a < end && store.add(a*2)) {
-					list.add(a*2);
-				}
-				if(a < end && store.add(a+1)) {
-					list.add(a+1);
-				}
-				
-			}
-			if(success)
-				break;
-		}
-		System.out.println(answer);
-		
-	}
-	
-	
-	/*
-	 경우의 수 
-	 1 더하기
-	 1 빼기
-	 2 곱하기
-	 */
 
+
+    public static void main(String[] args) throws Exception {
+        // 코드 작성
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[100001];
+
+        Arrays.fill(arr,Integer.MAX_VALUE);
+        arr[N] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
+
+        while(!queue.isEmpty()){
+            int cur = queue.poll();
+            if(cur == K)
+                break;
+
+            if(cur + 1 <= 100000 && arr[cur+1] > arr[cur] + 1 ){
+                arr[cur+1] = arr[cur] + 1;
+                queue.add(cur+1);
+            }
+            if(cur -1 >= 0 && arr[cur-1] > arr[cur] + 1){
+                arr[cur-1] = arr[cur] + 1;
+                queue.add(cur-1);
+
+            }
+            if(cur*2 <=100000 && arr[cur*2] > arr[cur] + 1 ){
+                arr[cur*2] = arr[cur] + 1;
+                queue.add(cur*2);
+
+            }
+        }
+
+        System.out.println(arr[K]);
+
+    }
 }
