d1314bbf8e3ac413171db64c30e71cf65e8b1b4c
uiseok <109773298+ohuiseok@users.noreply.github.com>
2024-02-25 21:23:01 +0900

    [Silver II] Title: 걷다보니 신천역 삼 (Small), Time: 136 ms, Memory: 15552 KB -BaekjoonHub

--- 백준/Silver/14650. 걷다보니 신천역 삼 （Small）/걷다보니 신천역 삼 （Small）.java
+++ 백준/Silver/14650. 걷다보니 신천역 삼 （Small）/걷다보니 신천역 삼 （Small）.java
@@ -1,22 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-
+    //https://www.acmicpc.net/problem/14650
     public static int n;
-    public static HashSet<Integer> list = new HashSet<>();
+    public static Set<Integer> list = new HashSet<>();
 
     public static void search(int number) {
         String digit = "" + number;
+
         if (digit.length() == n) {
-            if (number % 3 == 0 && number != 0) list.add(number);
+            if (number % 3 == 0 && number != 0) {
+                list.add(number);
+            }
             return;
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) { // 0 1 2
             if (number == 0 && i == 0) continue;
-            search(number*10 + i);
+            search(number * 10 + i);
         }
     }
 
