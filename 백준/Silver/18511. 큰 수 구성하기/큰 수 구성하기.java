8f41c5520d0724101ca3c620c4634c25fa7dfcd6
uiseok <109773298+ohuiseok@users.noreply.github.com>
2024-02-25 20:45:02 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 132 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.java
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.java
@@ -10,30 +10,31 @@ public class Main {
     public static int N, K, answer;
     public static int[] arr;
 
-    public static void recursion(int value) {
-        if (value > N) return;
+    public static void recursion(int value) {       //0  //7  //77 //777 //775 //771 //77 //75 //757 //755 //751
+        if (value > N) return;  // 비교할 값
         answer = Math.max(answer, value);
 
         for (int i = K - 1; i >= 0; i--) {
             recursion(value * 10 + arr[i]);
         }
+
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        answer = Integer.MIN_VALUE;
+        N = Integer.parseInt(st.nextToken()); // 비교할 값 <- 이것보다 작은 것 중에 `가장 큰 수`!!!
+        K = Integer.parseInt(st.nextToken()); // 앞으로 입력 받을 원소의 갯수
+        answer = Integer.MIN_VALUE;             //답
 
-        arr = new int[K];
+        arr = new int[K];                       //원소 모아둘 배열
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < K; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());      //원소를 배열에 넣었습니다.
         }
-
+        /////////////////////////////////
         Arrays.sort(arr);
 
         recursion(0);
